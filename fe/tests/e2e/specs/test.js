83cb14dd6f8bb510e456b368e3e21e7a40e2375a
Kyle Coberly <kyle.coberly@gmail.com>
2020-02-13 23:27:56 -0700

    Fixing circleci

--- fe/tests/e2e/specs/test.js
+++ fe/tests/e2e/specs/test.js
@@ -3,6 +3,7 @@
 describe("My First Test", () => {
   it("Visits the app root url", () => {
     cy.visit("/");
+    expect(true).to.be(true)
     //cy.get("li").should("have.length", 4);
   });
 });
